bed619da96969f3380351ff6fd47e426cfcacf0b
angular <1921934563@qq.com>
2019-06-29 18:30:54 +0800

    添加项目文件。

---
+++ OWManage/OW_DAL/DBHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace OWManage.OW_DAL
+{
+    public class DBHelper
+    {
+        private static string Sqlstring = ConfigurationManager.ConnectionStrings["SqlString"].ConnectionString.ToString();
+
+    }
+}
